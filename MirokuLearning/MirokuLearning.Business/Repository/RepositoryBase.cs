0003df2334ace32feebcdec46cc4c796179b3bcb
taufikfadjar <taufikfadjar@live.com>
2017-08-16 18:54:06 +0700

    Update

---
+++ MirokuLearning/MirokuLearning.Business/Repository/RepositoryBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MirokuLearning.Business.Repository
+{
+    public class RepositoryBase<T> where T: class
+    {
+        private DbSet<T> dbSet;
+
+        public RepositoryBase(DbContext context)
+        {
+            dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> GetQueryable()
+        {
+            return dbSet.AsQueryable();
+        }
+
+
+
+    }
+}
